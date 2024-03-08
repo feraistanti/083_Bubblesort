e2ce44a60af2a67d6089f4548ca3aa534179e6d6
feraistanti <fistanti551@gmail.com>
2024-03-08 16:21:35 +0700

    Add project files.

---
+++ BubleSort/BubleSort.cpp
@@ -0,0 +1,82 @@
+// 058_bubblesort.cpp : This file contains the 'main' function. program execution begins an
+//
+
+#include <iostream>#
+#include <string>
+using namespace std;
+
+int arr[20]; // Deklarasi variabel global array a dengan ukuran 20
+int n; // Deklarasi variabel global n untuk menyimpan banyaknya elemen array
+
+void input() {        //procedur untuk input
+
+	while (true) {
+
+		cout << "Masukan banyaknya elemen pada array : "; //output ke layar
+		cin >> n; //input dari pengguna
+		if (n <= 20) // jika n kurang dari atau sama dengan 20
+			break; // keluar dari loop
+		else {    // jika n lebih dari 20
+			cout << "\nArray dapatmempunyai maksimal 20 elemen.\n"; // output ke layar
+
+		}
+	}
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "====================" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ":";
+		cin >> arr[i];
+
+
+	}
+}
+
+void bubbleSortArray() {
+	int pass = 1;
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j = 1];
+				arr[j + 1] = temp;
+
+			}
+		}
+		pass = pass + 1;
+
+		cout << endl;
+	} while (pass <= n - 1);
+	
+}
+void display() {
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "Element Array yang Telah tersusun" << endl;
+	cout << "=================================" << endl;
+	cout << endl;
+
+	for (int j = 0; j < n; j++) {
+		cout << arr[j];
+		if (j < n - 1) {
+			cout << "--->";
+		}
+	}
+	cout << endl;
+	cout << endl;
+
+	cout << "jumlah pass = " << n - 1 << endl;
+	cout << endl;
+	cout << endl;
+}
+int main() {
+	input();
+
+	bubbleSortArray();
+	display();
+
+	return 0;
+}
