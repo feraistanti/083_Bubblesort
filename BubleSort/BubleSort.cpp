4f44c56a79b2f9d8996d1315d1f419683f493655
feraistanti <fistanti551@gmail.com>
2024-03-08 16:31:18 +0700

    Menambahkan procedur display

--- BubleSort/BubleSort.cpp
+++ BubleSort/BubleSort.cpp
@@ -52,3 +52,22 @@ void bubbleSortArray() {
 	} while (pass <= n - 1);
 
 }
+void display() {
+	cout << endl;
+	cout << "=================================" << endl;
+	cout << "Element Array yang Telah tersusun" << endl;
+	cout << "=================================" << endl;
+	cout << endl;
+
+	for (int j = 0; j < n; j++) {
+		cout << arr[j];
+		if (j < n - 1) {
+			cout << "--->";
+		}
+	}
+	cout << endl;
+	cout << endl;
+
+	cout << "jumlah pass = " << n - 1 << endl;
+	cout << endl;
+	cout << endl;
