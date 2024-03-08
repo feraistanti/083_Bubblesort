13a1896dcce10cf2173ebab9666117f8373837c6
feraistanti <fistanti551@gmail.com>
2024-03-08 16:30:28 +0700

    Membuat BubbleSort Array

--- BubleSort/BubleSort.cpp
+++ BubleSort/BubleSort.cpp
@@ -33,3 +33,22 @@ void input() {        //procedur untuk input
 
 	}
 }
+
+void bubbleSortArray() {
+	int pass = 1;
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int temp = arr[j];
+				arr[j] = arr[j = 1];
+				arr[j + 1] = temp;
+
+			}
+		}
+		pass = pass + 1;
+
+		cout << endl;
+	} while (pass <= n - 1);
+
+}
